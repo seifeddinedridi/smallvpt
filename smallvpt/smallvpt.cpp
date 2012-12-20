7573a064fd76661510f09835204ea092c056d527
Seifeddine Dridi <dexterous.programmer@gmail.com>
2012-12-20 02:54:46 -0800

    Added a pragma to disable double to float warning

--- smallvpt/smallvpt.cpp
+++ smallvpt/smallvpt.cpp
@@ -2,6 +2,7 @@
 #include <math.h>   // smallpt, a Path Tracer by Kevin Beason, 2008
 #include <stdlib.h> // Make : g++ -O3 -fopenmp smallpt.cpp -o smallpt
 #include <stdio.h>  //        Remove "-fopenmp" for g++ version < 4.2
+#pragma warning(disable: 4244) // Disable double to float warning
 namespace XORShift
 {
 	// XOR shift PRNG
@@ -149,7 +150,7 @@ Vec radiance(const Ray &r, int depth){
     radiance(reflRay,depth)*Re+radiance(Ray(x,tdir),depth)*Tr);
 }
 int main(int argc, char *argv[]){
-  int w=500, h=500, samps = argc==2 ? atoi(argv[1])/4 : 1; // # samples
+  int w=400, h=400, samps = argc==2 ? atoi(argv[1])/4 : 1; // # samples
   Ray cam(Vec(50,52,295.6), Vec(0,-0.042612,-1).norm()); // cam pos, dir
   Vec cx=Vec(w*.5135/h), cy=(cx%cam.d).norm()*.5135, r, *c=new Vec[w*h];
 #pragma omp parallel for schedule(dynamic, 1) private(r)       // OpenMP
@@ -168,7 +169,7 @@ int main(int argc, char *argv[]){
           c[i] = c[i] + Vec(clamp(r.x),clamp(r.y),clamp(r.z))*.25;
         }
   }
-  FILE *f = fopen("image.ppm", "w");         // Write image to PPM file.
+  FILE *f = fopen("..\\Renders\\image.ppm", "w"); // Write image to PPM file.
   fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
   for (int i=0; i<w*h; i++)
     fprintf(f,"%d %d %d ", toInt(c[i].x), toInt(c[i].y), toInt(c[i].z));
